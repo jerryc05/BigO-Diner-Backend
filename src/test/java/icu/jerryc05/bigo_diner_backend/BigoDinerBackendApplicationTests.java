0308e3e69df0c33eddbf013efad12389d1da7ed4
Jerry Chen <ziyan.chen@messagegears.com>
2023-10-15 12:22:13 -0400

    init commit

---
+++ src/test/java/icu/jerryc05/bigo_diner_backend/BigoDinerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.jerryc05.bigo_diner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigoDinerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
