51bb0ad7e26d0ec2640798a57111cd0906c7416f
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-10-24 17:20:25 -0400

    init commit

---
+++ src/main/java/icu/jerryc05/bigo_diner_backend/controller/interceptor/RefreshTokenInterceptor.java
@@ -0,0 +1,7 @@
+package icu.jerryc05.bigo_diner_backend.controller.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class RefreshTokenInterceptor implements HandlerInterceptor {
+
+}
